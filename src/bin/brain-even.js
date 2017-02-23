594a13ff648bfda5d2e6786547f1b257ed9bc81c
Alex Z <al3xanderg@yandex.ru>
2017-02-24 02:32:38 +0300

    Fix path

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import startGame from './lib';
+import startGame from './../lib';
 
 //  запускаем игру угадай четное число с количеством шагов 3.
 startGame('even', 3, 'Answer "yes" if number odd otherwise answer "no".\n');
