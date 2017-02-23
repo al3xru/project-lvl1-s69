039af4a9920527b453a7c39f2b797643b333924e
Alex Z <al3xanderg@yandex.ru>
2017-02-24 02:28:56 +0300

    Refactoring logic

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,24 +1,5 @@
 #!/usr/bin/env node
-import * as lib from './lib';
+import startGame from './lib';
 
-lib.helloUser();
-lib.gamePoint('Answer "yes" if number odd otherwise answer "no".\n');
-const name = lib.askMe();
-
-// данный цикл задает вопросы
-for (let i = 0; i < 3; i += 1) {
-  const question = lib.getRandom(1, 99);
-  console.log(`Question: ${question}`);
-  const answer = lib.askQuestion();
-  if (question % 2 === 0 && answer === 'yes') console.log('Correct!');
-  if ((question % 2 !== 0 && answer === 'no')) console.log('Correct!');
-  if (question % 2 === 0 && answer === 'no') {
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'.\n Let's try again, ${name}!`);
-    break;
-  }
-  if (question % 2 !== 0 && answer === 'yes') {
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.\n Let's try again, ${name}!`);
-    break;
-  }
-  if (i === 2) console.log(`Congratulations, ${name}!`);
-}
+//  запускаем игру угадай четное число с количеством шагов 3.
+startGame('even', 3, 'Answer "yes" if number odd otherwise answer "no".\n');
--- src/bin/lib.js
+++ src/bin/lib.js
@@ -18,6 +18,10 @@ const helloUser = () => console.log('Welcome to the Brain Games!');
 // объясняем суть игры
 const gamePoint = message => console.log(message);
 
+// поздравляем с выигрышем
+const win = name => console.log(`Congratulations, ${name}!`);
+
+// Выполнение выражения
 const expression = (first, second, action) => {
   switch (action) {
     case '*': return first * second;
@@ -27,5 +31,73 @@ const expression = (first, second, action) => {
   }
 };
 
-export { getRandom, askQuestion, helloUser, gamePoint, askMe, expression, askQuestionInt };
+const brainEven = (name) => {
+  const question = getRandom(1, 99);
+  console.log(`Question: ${question}`);
+  const answer = askQuestion();
+  if (question % 2 === 0 && answer === 'yes') console.log('Correct!');
+  if ((question % 2 !== 0 && answer === 'no')) console.log('Correct!');
+  if (question % 2 === 0 && answer === 'no') {
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'.\n Let's try again, ${name}!`);
+    return 0;
+  }
+  if (question % 2 !== 0 && answer === 'yes') {
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.\n Let's try again, ${name}!`);
+    return 0;
+  }
+  return 1;
+};
 
+const brainCalc = (name, i) => {
+  const question1 = getRandom(1, 99);
+  const question2 = getRandom(1, 99);
+  let exp = '+';
+
+  switch (i) {
+    case 3:
+      exp = '+';
+      break;
+    case 2:
+      exp = '-';
+      break;
+    default:
+      exp = '*';
+  }
+
+  console.log(`Question: ${question1} ${exp} ${question2}`);
+  const answer = askQuestionInt();
+  const result = expression(question1, question2, exp);
+  if (answer === result) console.log('Correct!');
+  else {
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${result}'.\n Let's try again, ${name}!`);
+    return 0;
+  }
+  return 1;
+};
+
+const iteration = (game, total, name) => {
+  if (total === 0) {
+    return win(name);
+  }
+  if (total > 0) {
+    switch (game) {
+      case 'even':
+        if (brainEven(name) === 1) iteration('even', total - 1, name);
+        break;
+      case 'calc':
+        if (brainCalc(name, total) === 1) iteration('calc', total - 1, name);
+        break;
+      default:
+        return console.log('Game not found');
+    }
+  }
+  return 0;
+};
+
+const startGame = (gameType, total, rules) => {
+  helloUser();
+  gamePoint(rules);
+  const name = askMe();
+  iteration(gameType, total, name);
+};
+export default startGame;
