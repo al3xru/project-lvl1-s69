fd97bd0ad6f6963eb8e36ce1344c6754bed1c039
Alex Z <al3xanderg@yandex.ru>
2017-02-26 15:46:11 +0300

    fix import

--- src/flow.js
+++ src/flow.js
@@ -1,20 +1,20 @@
 import { car, cdr } from 'hexlet-pairs';
-import * as lib from './lib';
+import { helloUser, askMe, win, askQuestion } from './lib';
 
 const startGame = (game, questionAnswer) => {
   // приветствуем юзера и объясняем игру
-  lib.helloUser();
+  helloUser();
   console.log(`${game}`);
   // спрашиваем имя
-  const name = lib.askMe();
+  const name = askMe();
   // кол-во вопросов
   const totalSteps = 3;
 
   const iteration = (step) => {
-    if (step === totalSteps) return lib.win(name);
+    if (step === totalSteps) return win(name);
     const actQuestion = questionAnswer();
     console.log(`Question: ${car(actQuestion)}`);
-    const answer = lib.askQuestion();
+    const answer = askQuestion();
     const trueAnswer = cdr(actQuestion);
     if (answer === trueAnswer) return iteration(step + 1);
     console.log(`"${answer}" is wrong answer ;(. Correct answer was "${trueAnswer}".`);
